9ca369d1dc46f202ec720086cf746d47e921b1f2
NereonNeo <nodirayakubova26@gmail.com>
2021-07-07 17:34:27 +0500

    add sorting and install redux

--- src/component/Shop/PageContainer.js
+++ src/component/Shop/PageContainer.js
@@ -10,10 +10,10 @@ const PageContainer = () => {
             img: 'https://cdn.fishki.net/upload/post/2017/02/22/2225118/vse-lyudi-goda-s-oblozhki-time-1.jpg',
             hoverImg:
                 'https://regnum.ru/uploads/pictures/news/2016/04/22/regnum_picture_1461285953_normal.jpg',
-            term: 'NEW',
+            term: 'Free',
             category: 'Business',
             title: 'The way of Donald Trump',
-            price: 15.0,
+            price: null,
             rate: 4,
             reviews: 3,
             id: 1,
@@ -22,10 +22,10 @@ const PageContainer = () => {
             img: 'https://theblueprint.ru/upload/14094m/aaad24c47ab65761ea62a2dd3cfc0f72.jpg',
             hoverImg:
                 'https://theblueprint.ru/upload/14094m/aaad24c47ab65761ea62a2dd3cfc0f72.jpg',
-            term: 'Popular',
+            term: 'Paid',
             category: 'Fashion',
             title: 'Vogue',
-            price: 215.0,
+            price: 45.0,
             rate: 5,
             reviews: 10,
             id: 2,
@@ -34,10 +34,10 @@ const PageContainer = () => {
             img: 'https://media.lpgenerator.ru/uploads/2016/03/30/1____.jpg',
             hoverImg:
                 'https://media.lpgenerator.ru/uploads/2016/03/30/1____.jpg',
-            term: 'NEW',
+            term: 'Free',
             category: 'Woman',
             title: 'Bolt Sweatshirt',
-            price: 45.0,
+            price: null,
             rate: 4,
             reviews: 3,
             id: 3,
@@ -46,7 +46,7 @@ const PageContainer = () => {
             img: 'https://static.tildacdn.com/tild3439-3135-4337-b832-613264373833/-Nature-768x1009.jpg',
             hoverImg:
                 'https://static.tildacdn.com/tild3439-3135-4337-b832-613264373833/-Nature-768x1009.jpg',
-            term: 'NEW',
+            term: 'Paid',
             category: 'Man',
             title: 'Bolt Sweatshirt ssss',
             price: 35.0,
@@ -58,10 +58,10 @@ const PageContainer = () => {
             img: 'https://static.tildacdn.com/tild3439-3135-4337-b832-613264373833/-Nature-768x1009.jpg',
             hoverImg:
                 'https://static.tildacdn.com/tild3439-3135-4337-b832-613264373833/-Nature-768x1009.jpg',
-            term: 'NEW',
+            term: 'Free',
             category: 'XZ',
             title: 'Bolt Sweatshirt TTTTT',
-            price: 52.0,
+            price: null,
             rate: 3,
             reviews: 10,
             id: 5,
@@ -70,7 +70,7 @@ const PageContainer = () => {
             img: 'https://static.tildacdn.com/tild3439-3135-4337-b832-613264373833/-Nature-768x1009.jpg',
             hoverImg:
                 'https://static.tildacdn.com/tild3439-3135-4337-b832-613264373833/-Nature-768x1009.jpg',
-            term: 'NEW',
+            term: 'Paid',
             category: 'XZ',
             title: 'Bolt Sweatshirt RRRR',
             price: 305.0,
@@ -80,11 +80,20 @@ const PageContainer = () => {
         },
     ];
     //! one logic for all
+
     if (sort == 'plus') {
         dataFrom.sort((a, b) => +a.price - +b.price);
-    } else {
+    }
+    if (sort == 'minus') {
         dataFrom.sort((a, b) => +b.price - +a.price);
     }
+    if (sort == 'free') {
+        dataFrom.sort((a) => a.term !== 'Free');
+    }
+    if (sort == 'paid') {
+        dataFrom.sort((a) => a.term !== 'Paid');
+    }
+
     return (
         <div>
             <PageInfo sort={sort} setSort={setSort} />
