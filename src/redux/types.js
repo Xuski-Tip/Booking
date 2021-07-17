d0d55c947c7954a992b3c049de17d49119079f8c
Xuski-Tip <billkiller.com@gmail.com>
2021-07-17 14:14:37 +0500

    main-appdate

--- src/redux/types.js
+++
@@ -1,5 +0,0 @@
-export const GET_MAGAZINE_CARDS = 'GET_MAGAZINE_CARDS';
-export const SORT_PLUS = 'SORT_PLUS';
-export const SORT_MINUS = 'SORT_MINUS';
-export const SORT_FREE = 'SORT_FREE';
-export const SORT_PAID = 'SORT_PAID';
