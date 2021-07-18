27e5468f0340847c95452ae1f84d3512fc4f2aec
20010308 <xolmuminovumidjon0@gmail.com>
2021-07-18 21:48:09 +0500

    Add master

--- src/component/Subscribe.js
+++ src/component/Subscribe.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import {Modal, ModalBody, ModalFooter} from "reactstrap"
 import {AvForm, AvField} from "availity-reactstrap-validation"
+import FooterInfo from "./FooterInfo";
+import Header from "./Header";
+import HeaderUpper from "./HeaderUpper";
 
 const Subscribe = () => {
 
@@ -30,65 +33,89 @@ const Subscribe = () => {
         console.log(summ)
     };
     return (
-        <div className="container mt-5 jurnal">
-            <div className="d-flex justify-content-around">
-                <button type="button" className="btn btn-outline-danger" onClick={showModal1}>
-                    An'anaviy usulda obuna
-                </button>
-                <button type="button" className="btn btn-outline-primary" onClick={showModal2}>
-                    Online usulda obuna
-                </button>
+        <>
+            <Header/>
+            <HeaderUpper/>
+            <div className="jurnal">
+
+                <div className="container mt-5">
+                    <div className="d-flex justify-content-around">
+                        <button type="button" className="btn btn-outline-danger" onClick={showModal1}>
+                            An'anaviy usulda obuna
+                        </button>
+                        <button type="button" className="btn btn-outline-primary" onClick={showModal2}>
+                            Online usulda obuna
+                        </button>
+                    </div>
+
+                    <Modal isOpen={open1} toggle={showModal1}>
+                        <AvForm>
+                            <ModalBody>
+                                <AvField name="name" required label='F.I.SH' type="text"/>
+                                <AvField name="adres" required label="Adres" type="text"/>
+                                <AvField name="email" required label="Email" type="text"/>
+                                <AvField name="number" required label="Telefon nomer" type="number"/>
+                                <div className="d-flex justify-content-between">
+                                    <AvField type="select" className="w-75" required name="jurnal"
+                                             label="Jurnalni tanlang">
+                                        <option value="1">2021-yil 1-son</option>
+                                        <option value="2">2021-yil 2-son</option>
+                                        <option value="3">2021-yil 3-son</option>
+                                        <option value="4">2021-yil 4-son</option>
+                                    </AvField>
+                                    <AvField name="nusxa" required type="number" label="Necha nusxada"/>
+                                </div>
+
+                            </ModalBody>
+                            <ModalFooter className="d-flex justify-content-between">
+                                <button type="button" className="btn btn-success">To'lov</button>
+                                <button type="button" className="btn btn-success" onClick={showModal1}>Cansel</button>
+                            </ModalFooter>
+                        </AvForm>
+                    </Modal>
+
+
+                    <Modal isOpen={open2} toggle={showModal2}>
+                        <AvForm onSubmit={buy}>
+                            <ModalBody>
+                                <AvField name="name" required label='F.I.SH' type="text"/>
+                                {/*<AvField name="adres" required label="Adres" type="text"/>*/}
+                                <AvField name="email" required label="Email" type="text"/>
+                                <AvField name="number" label="Telefon nomer" type="number"/>
+                                <div className="">
+                                    <div className="row">
+                                        <div className="col-6">
+                                            <AvField type="checkbox" label="Jurnal1" className="w-75"
+                                                     name="jurnal1"></AvField>
+                                        </div>
+                                        <div className="col-6">
+                                            <AvField type="checkbox" label="Jurnal2" className="w-75"
+                                                     name="jurnal2"></AvField>
+                                        </div>
+                                        <div className="col-6">
+                                            <AvField type="checkbox" label="Jurnal3" className="w-75"
+                                                     name="jurnal3"></AvField>
+                                        </div>
+                                        <div className="col-6">
+                                            <AvField type="checkbox" label="Jurnal4" className="w-75"
+                                                     name="jurnal4"></AvField>
+                                        </div>
+                                    </div>
+                                    <AvField name="nusxa" required type="number" label="Necha nusxada"/>
+                                </div>
+
+                            </ModalBody>
+                            <ModalFooter className="d-flex justify-content-between">
+                                <button type="submit" className="btn btn-success">To'lov</button>
+                                <button type="button" className="btn btn-success" onClick={showModal2}>Cansel</button>
+                            </ModalFooter>
+                        </AvForm>
+                    </Modal>
+                </div>
+
             </div>
-
-            <Modal isOpen={open1} toggle={showModal1}>
-                <AvForm>
-                    <ModalBody>
-                        <AvField name="name" required label='F.I.SH' type="text"/>
-                        <AvField name="adres" required label="Adres" type="text"/>
-                        <AvField name="email" required label="Email" type="text"/>
-                        <AvField name="number" required label="Telefon nomer" type="number"/>
-                        <div className="d-flex justify-content-between">
-                            <AvField type="select" className="w-75" required name="jurnal" label="Jurnalni tanlang">
-                                <option value="1">2021-yil 1-son</option>
-                                <option value="2">2021-yil 2-son</option>
-                                <option value="3">2021-yil 3-son</option>
-                                <option value="4">2021-yil 4-son</option>
-                            </AvField>
-                            <AvField name="nusxa" required type="number" label="Necha nusxada"/>
-                        </div>
-
-                    </ModalBody>
-                    <ModalFooter className="d-flex justify-content-between">
-                        <button type="button" className="btn btn-success">To'lov</button>
-                        <button type="button" className="btn btn-success" onClick={showModal1}>Cansel</button>
-                    </ModalFooter>
-                </AvForm>
-            </Modal>
-
-
-            <Modal isOpen={open2} toggle={showModal2}>
-                <AvForm onSubmit={buy}>
-                    <ModalBody>
-                        <AvField name="name" required label='F.I.SH' type="text"/>
-                        {/*<AvField name="adres" required label="Adres" type="text"/>*/}
-                        <AvField name="email" required label="Email" type="text"/>
-                        <AvField name="number" label="Telefon nomer" type="number"/>
-                        <div className="">
-                            <AvField type="checkbox" label="Jurnal1" className="w-75" name="jurnal1"></AvField><br/>
-                            <AvField type="checkbox" label="Jurnal2" className="w-75" name="jurnal2"></AvField><br/>
-                            <AvField type="checkbox" label="Jurnal3" className="w-75" name="jurnal3"></AvField><br/>
-                            <AvField type="checkbox" label="Jurnal4" className="w-75" name="jurnal4"></AvField><br/>
-                            <AvField name="nusxa" required type="number" label="Necha nusxada"/>
-                        </div>
-
-                    </ModalBody>
-                    <ModalFooter className="d-flex justify-content-between">
-                        <button type="submit" className="btn btn-success">To'lov</button>
-                        <button type="button" className="btn btn-success" onClick={showModal2}>Cansel</button>
-                    </ModalFooter>
-                </AvForm>
-            </Modal>
-        </div>
+            <FooterInfo/>
+        </>
     )
 
 }
