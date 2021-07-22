58f63e8b682e9168ab8205d12970f9791182069c
Xuski-Tip <billkiller.com@gmail.com>
2021-07-22 21:26:54 +0500

    nothing2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React, { Suspense } from "react";
+import React from "react";
 import "./css/index.scss";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"
 import Router from "./component/Router";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { rootReducer } from "./redux/rootReducer";
 import "./component/i18next";
+import { Suspense } from "react";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <Router />
-  </Suspense>,
-  document.querySelector("#root")
+    <Provider store={store}>
+        <Suspense fallback={(<div>Loading...</div>)}><Router /></Suspense>
+    </Provider>
+
+, document.querySelector("#root")
 );
