c3dfd977dbe0045772dbdaa64b9cefee458095e1
20010308 <xolmuminovumidjon0@gmail.com>
2021-07-28 12:29:18 +0500

    aaaaaa

--- src/component/RecentWorkSlide.js
+++ src/component/RecentWorkSlide.js
@@ -5,59 +5,54 @@ import {API} from "../simpleJs/loginApi";
 
 const RecentWorkSlide = () => {
 
-  const [jurnal12, setJurnal12] = useState([]);
+    const [jurnal12, setJurnal12] = useState([]);
 
-  useEffect(() => {
-    axios.get(API + "magazine")
-        .then((res) => {
-          console.log("salom");
-          console.log(res);
-          setJurnal12(res.data.magazine.splice(0, 12));
-        })
-  }, []);
+    useEffect(() => {
+        axios.get(API + "magazine")
+            .then((res) => {
+                console.log("salom");
+                console.log(res);
+                setJurnal12(res.data.magazine.splice(0, 12));
+            })
+    }, []);
 
 
-  return (
-      <div className="portfolio">
-        <div id="portfolio" className="portfolio-6-columns" data-margin="0">
-          {jurnal12.map((item) => {
-            return (
-              <div key={item.id}>
-                <div
-                  className="portfolio-item img-zoom ct-photography ct-marketing ct-media"
-                  key={item.id}
-                >
-                  <div className="portfolio-item-wrap">
-                    <div className="portfolio-image">
-                      <a href="!#">
-                        <img
-                          className="portfolio-img"
-                          src={"https://paycom-test.napaautomotive.uz/storage/" +item.image}
-                          alt=""
-                        />
-                      </a>
-                      <h6 className="mt-2">{item.title_uz}</h6>
-                    </div>
-                    <div className="portfolio-description">
-                      <a
-                        title="Paper Pouch!"
-                        data-lightbox="image"
-                        href={"https://paycom-test.napaautomotive.uz/storage/" +item.image}
-                      >
-                        {/*<i className="icon-maximize"></i>*/}
-                      </a>
-                      <a target="_blank" href={element.pdf}>
-                        <i className="icon-link"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+    return (
+        <div className="portfolio">
+            <div id="portfolio" className="portfolio-6-columns" data-margin="0">
+                {jurnal12.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <div
+                                className="portfolio-item img-zoom h-100 ct-photography ct-marketing ct-media"
+                                key={item.id}
+                            >
+                                <div className="portfolio-item-wrap">
+                                    <div className="portfolio-image">
+                                        <a href="!#">
+                                            <img
+                                                className="portfolio-img"
+                                                src={"https://paycom-test.napaautomotive.uz/storage/" + item.image}
+                                                alt=""
+                                            />
+                                        </a>
+                                        <h6 className="mt-2">{item.title_uz}</h6>
+                                    </div>
+                                    <div className="portfolio-description">
+                                        <a
+                                            title="Paper Pouch!"
+                                            data-lightbox="image"
+                                            href={"https://paycom-test.napaautomotive.uz/storage/" + item.image}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-      </div>
     );
 
-}
+};
 export default RecentWorkSlide;
